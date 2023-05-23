a8e0ae31ff177703da760bb14899d6fe24698844
AmeyPank <pankantiameya@gmail.com>
2023-05-24 02:37:46 +0530

    const fields

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import Form from './SignUp';
 
 const App = () => {
+  const handleSubmit = (formValues) => {
+    console.log(formValues);
+    // Perform further actions with the form values
+  };
+
+  const fields = [
+    { name: 'firstName', type: 'text', label: 'First Name' },
+    { name: 'lastName', type: 'text', label: 'Last Name' },
+    { name: 'email', type: 'email', label: 'Email' },
+    { name: 'password', type: 'password', label: 'Password' }
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Form Example</h1>
+      <Form onSubmit={handleSubmit} fields={fields} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
